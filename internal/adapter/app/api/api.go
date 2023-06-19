2ce55c015a335d6fc663bca456d630ec52bbd046
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 <lautaroolmedo77@gmail.com>
2023-06-18 22:15:43 -0300

    Add Added port dependent injection to the adapter. To the application layer

--- internal/adapter/app/api/api.go
+++ internal/adapter/app/api/api.go
@@ -1,46 +1,70 @@
 package api
 
-// AL ESTAR EN EL ADAPTADOR DE "APPLICATION" IMPORTAMOS EL "DOMINIO"
 import (
 	ports "Arithmetic/internal/ports"
 )
 
 type Adapter struct {
-	arith ports.ArithmeticPort
+	db         ports.DbPort
+	arithmetic ports.ArithmeticPort
 }
 
-func NewAdapter(arith ports.ArithmeticPort) *Adapter { // ---> Inyeccion de dependencia
-	return &Adapter{arith: arith}
+func NewAdapter(db ports.DbPort, arithmetic ports.ArithmeticPort) *Adapter {
+	return &Adapter{db: db, arithmetic: arithmetic}
 }
 
 func (apia Adapter) GetAddition(a, b int32) (int32, error) {
-	answer, err := apia.arith.Addition(a, b)
+	answer, err := apia.arithmetic.Addition(a, b)
 	if err != nil {
 		return 0, err
 	}
+
+	err = apia.db.AddToHistory(answer, "addition")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
 func (apia Adapter) GetSubtraction(a, b int32) (int32, error) {
-	answer, err := apia.arith.Subtraction(a, b)
+	answer, err := apia.arithmetic.Subtraction(a, b)
 	if err != nil {
 		return 0, err
 	}
+
+	err = apia.db.AddToHistory(answer, "subtraction")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
 func (apia Adapter) GetMultiplication(a, b int32) (int32, error) {
-	answer, err := apia.arith.Multiplication(a, b)
+	answer, err := apia.arithmetic.Multiplication(a, b)
 	if err != nil {
 		return 0, err
 	}
+
+	err = apia.db.AddToHistory(answer, "multiplication")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
 func (apia Adapter) GetDivision(a, b int32) (int32, error) {
-	answer, err := apia.arith.Division(a, b)
+	answer, err := apia.arithmetic.Division(a, b)
 	if err != nil {
 		return 0, err
 	}
+
+	err = apia.db.AddToHistory(answer, "division")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
